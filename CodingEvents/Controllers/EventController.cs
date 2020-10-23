68488c4d6155b609f0b72da03f02a53394b7774a
RahulFernando <rahulrulz680@gmail.com>
2020-10-23 15:34:11 +0530

    init

---
+++ CodingEvents/Controllers/EventController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/event/add")]
+        public IActionResult Add(string name, string description)
+        {
+            EventData.Add(new Event(name, description));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (var eventId in eventIds)
+                EventData.Remove(eventId);
+
+            return RedirectToAction("Index"); 
+        }
+
+    }
+}
---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        private static int NextId = 1;
+
+        public Event(string name, string description)
+        {
+            Id = NextId;
+            Name = name;
+            Description = description;
+            NextId++;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var @event = obj as Event;
+            return @event != null &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
